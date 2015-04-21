7611a76a96386a4f314bbf4b071981dba9aaf2bd
Gabriel Buades <gbuades@soffid.com>
2015-04-21 12:25:39 +0200

    [LOG-134] Set also subject context on ZK threads

--- src/main/java/es/caib/zkib/zkiblaf/jboss/JBossEventThreadInit.java
+++ src/main/java/es/caib/zkib/zkiblaf/jboss/JBossEventThreadInit.java
@@ -2,6 +2,8 @@ package es.caib.zkib.zkiblaf.jboss;
 
 import java.security.Principal;
 
+import javax.security.auth.Subject;
+
 import org.jboss.security.SecurityAssociation;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.Event;
@@ -9,7 +11,8 @@ import org.zkoss.zk.ui.event.EventThreadInit;
 
 public class JBossEventThreadInit implements EventThreadInit {
     private Principal _principal;    
-    private Object _credential;    
+    private Object _credential;
+	private Subject _subject;    
     /** Retrieve info at the constructor, which runs at the servlet thread. */    
     public JBossEventThreadInit() {    
     }    
@@ -17,11 +20,11 @@ public class JBossEventThreadInit implements EventThreadInit {
     /** Initial the event processing thread at this method. */    
 	public void prepare(Component comp, Event event) throws Exception {
         _principal = SecurityAssociation.getPrincipal();        
-        _credential = SecurityAssociation.getCredential();        
+        _credential = SecurityAssociation.getCredential();      
+        _subject = SecurityAssociation.getSubject();
 	}
 	public boolean init(Component comp, Event event) throws Exception {
-        SecurityAssociation.setPrincipal(_principal);        
-        SecurityAssociation.setCredential(_credential);        
+		SecurityAssociation.pushSubjectContext(_subject, _principal, _credential);
 		return true;
 	}    
 
