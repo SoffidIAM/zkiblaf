819f2e6dd3adb58fed9820fb43d903ca16a7ae07
Gabriel Buades <gbuades@soffid.com>
2015-04-23 13:09:04 +0200

    [LOG-132] Change export method

--- src/main/java/es/caib/zkib/zkiblaf/export/ExportContingutMime.java
+++ src/main/java/es/caib/zkib/zkiblaf/export/ExportContingutMime.java
@@ -3,8 +3,10 @@ package es.caib.zkib.zkiblaf.export;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
+import org.zkoss.util.media.AMedia;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zul.Filedownload;
 
 /**
  * Exportem el contingut mitjançant el servlet ContentExportServlet
@@ -15,33 +17,8 @@ import org.zkoss.zk.ui.util.Clients;
  */
 public class ExportContingutMime {
 	
-	public static void export (String mime, String contingut, String filename, Component c) {
-		try {
-			mime = URLEncoder.encode(mime, "UTF-8"); //$NON-NLS-1$
-			contingut = URLEncoder.encode(contingut, "UTF-8"); //$NON-NLS-1$
-		} catch (UnsupportedEncodingException e) {
-			return;
-		}
-		
-		zkForm form = new zkForm();
-		form.setDynamicProperty("action", "./contentexport/"+filename); //$NON-NLS-1$ //$NON-NLS-2$
-		form.setDynamicProperty("target", "_blank"); //$NON-NLS-1$ //$NON-NLS-2$
-		form.setDynamicProperty("method", "post"); //$NON-NLS-1$ //$NON-NLS-2$
-		form.setPage(c.getPage());
-		
-		zkInput i_mime = new zkInput();
-        i_mime.setParent(form);
-        i_mime.setDynamicProperty("type", "hidden"); //$NON-NLS-1$ //$NON-NLS-2$
-        i_mime.setDynamicProperty("name", "mime"); //$NON-NLS-1$ //$NON-NLS-2$
-        i_mime.setValue(mime);
-        
-        zkInput i_contingut = new zkInput();
-        i_contingut.setParent(form);
-        i_contingut.setDynamicProperty("type", "hidden"); //$NON-NLS-1$ //$NON-NLS-2$
-        i_contingut.setDynamicProperty("name", "contingut"); //$NON-NLS-1$ //$NON-NLS-2$
-        i_contingut.setValue(contingut);
-		
-		Clients.submitForm(form);		
+	public static void export (String mime, String contingut, String filename, Component c) throws UnsupportedEncodingException {
+		Filedownload.save(new AMedia(filename, null, mime, contingut.getBytes("UTF-8")));
 	}
 
 }
